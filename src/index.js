9407c66168a6ecc8ec91b2cb73467b7008120b40
Lidii <lidiiany92@gmail.com>
2019-03-13 13:50:10 -0400

    Initialized index.js with Router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route path="/" component={App}/>
+        </Switch>
+    </Router>
+);
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();

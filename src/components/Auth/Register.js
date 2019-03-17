95af15c6049f5a82ce75692c8d637868ea25693b
Lidii <lidiiany92@gmail.com>
2019-03-17 15:33:26 -0400

    Added form validation functionality

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -9,8 +9,44 @@ class Register extends React.Component {
         username: '',
         email: '',
         password: '',
-        passwordConfirmation: ''
+        passwordConfirmation: '',
+        errors: []
     }
+
+
+    isFormValid = () => {
+        let errors = [];
+        let error;
+
+        if(this.isFormEmpty(this.state)){
+            error = {message: "Please fill in all fields"}
+            this.setState({errors: errors.concat(error)});
+            return false; 
+        } else if (!this.isPasswordValid(this.state)) {
+            error = {message: "Password is invalid"};
+            this.setState({errors: errors.concat(error)});
+            return false;     
+        } else {
+            return true;
+        }
+    }
+
+    isFormEmpty = ({username, email, password, passwordConfirmation}) => {
+        return !username.length || !email.length || !password.length || !passwordConfirmation.length
+    }
+
+    isPasswordValid = ({password, passwordConfirmation}) => {
+        if(password.length < 6 || passwordConfirmation.length < 6) {
+            return false;
+        } else if (password !== passwordConfirmation) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    displayErrors = errors => errors.map((error, i) => <p key={i}>{error.message}</p>)
 
     handlechange = (event) => {
         this.setState({
@@ -19,20 +55,22 @@ class Register extends React.Component {
     }
 
     handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const createdUser = await firebase
-                .auth()
-                .createUserWithEmailAndPassword(this.state.email, this.state.password);
-                console.log(createdUser);
-        } catch(err) {
-            console.error(err);
+        if(this.isFormValid()) {
+            event.preventDefault();
+            try {
+                const createdUser = await firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(this.state.email, this.state.password);
+                    console.log(createdUser);
+            } catch(err) {
+                console.error(err);
+            }
         }
         
     }
 
     render () {
-        const {username, email, password, passwordConfirmation } = this.state;
+        const {username, email, password, passwordConfirmation, errors } = this.state;
         return (
            <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{maxWidth: 450}}>
@@ -54,6 +92,12 @@ class Register extends React.Component {
                             <Button color="orange" fluid size="large">Submit</Button>
                         </Segment>
                     </Form>
+                    {errors.length > 0 && (
+                        <Message error>
+                        <h3>Error</h3>
+                        {this.displayErrors(errors)}
+                        </Message>
+                    )}
                     <Message>Already a user? <Link to="/login">Login</Link></Message>
                 </Grid.Column>
            </Grid>

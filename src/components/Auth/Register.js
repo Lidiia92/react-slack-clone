a537102ec7b5f2940410a1347cf22951404680cf
Lidii <lidiiany92@gmail.com>
2019-03-19 13:05:54 -0400

    Created Login component

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -75,6 +75,15 @@ class Register extends React.Component {
                 })
                 .then(() => {
                   this.saveUser(createdUser).then(() => {
+                    this.setState({
+                        ...this.state,
+                        loading: false,
+                        username: '',
+                        email: '',
+                        password: '',
+                        passwordConfirmation: '',
+                        err: []
+                    });
                     console.log("user saved");
                   });
                 })
@@ -112,7 +121,7 @@ class Register extends React.Component {
         return (
            <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{maxWidth: 450}}>
-                    <Header as="h2" icon color="orange" textAlign="center">
+                    <Header as="h1" icon color="orange" textAlign="center">
                         <Icon name="puzzle piece" color="orange" />
                         Register for DevChat
                     </Header>

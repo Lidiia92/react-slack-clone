30232db1d75036fd04d95e87a310447d56e1a0ca
Lidii <lidiiany92@gmail.com>
2019-03-21 16:50:28 -0400

    Updated App.js with new components

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+import React  from 'react';
+import {Grid, Segment} from 'semantic-ui-react';
 import './App.css';
+import ColorPanel from './ColorPanel/ColorPanel';
+import SidePanel from './SidePanel/SidePanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanel/MetaPanel';
 
-class App extends Component {
-  render() {
-    return (
-      <div >
-       App
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Grid columns='equal' className="app" style={{background: "#eee"}}>
+
+	<Grid.Column>
+       	<ColorPanel />
+        <SidePanel />
+	</Grid.Column>	
+
+    <Grid.Column style={{marginLeft: 320, marginTop: 15}}>
+        	<Messages />
+	</Grid.Column>
+	  
+    <Grid.Column width={4} style={{marginTop: 15}}>
+        	<MetaPanel />
+    </Grid.Column>
+  </Grid>
+);
+
 
 export default App;

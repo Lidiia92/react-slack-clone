c70004393315da0df2184ab53a30f957130bb9d7
Lidii <lidiiany92@gmail.com>
2019-03-24 12:54:42 -0400

    Added App Header and User Dropdown

---
+++ src/components/SidePanel/UserPanel.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Grid, Header, Icon, Dropdown} from 'semantic-ui-react';
+
+class UserPanel extends React.Component {
+
+    dropdownOptions = () => [
+        {
+            key: "user",
+            text: <span>Signed in as <strong>User</strong></span>,
+            disabled: true
+        },
+
+        {
+            key: "avatar",
+            text: <span>Change Avatar</span>
+        },
+
+        {
+            key: "signout",
+            text: <span>Sign Out</span>
+        }
+    ]
+
+    render () {
+        return (
+            <Grid style={{background: '#4c3c4c', width: '100%'}}>
+                <Grid.Column style={{paddingLeft: '2em'}}>
+                    <Grid.Row style={{padding: '1.2em', margin: 0}}>
+                        {/* App Header */}
+                        <Header inverted floated="left" as="h2">
+                            <Icon name="code"/>
+                            <Header.Content style={{fontSize: '2rem'}}>DevChat</Header.Content>
+                        </Header>
+                    </Grid.Row>
+
+                    {/* User Dropdown */}
+                    <Header style={{padding: '0.25rem'}} as="h4" inverted>
+                        <Dropdown trigger={<span>User</span>} options={this.dropdownOptions()}/>
+                    </Header>
+                </Grid.Column>
+            </Grid>
+        );
+    }
+}
+
+export default UserPanel
